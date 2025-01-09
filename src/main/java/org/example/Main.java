6d67158d6cc85952db75f329849bae486ac1f935
Evgeniy Mironov <eemirono@inno.tech>
2025-01-09 10:57:36 +0300

    код который вычисляет и выводит в консоль сумму, разность, произведение и частное двух чисел

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число: ");
+        System.out.println("Введите первое число: ");
         int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
         int secondNumber = new Scanner(System.in).nextInt();
         double quotient = (double) firstNumber / secondNumber;
         System.out.println(quotient);
